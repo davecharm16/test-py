d5dc4c4936436438ca430fcbc0b12283de8d6d47
Dave Charm Bulaquena <davecharm16@gmail.com>
2024-10-10 11:24:59 +0800

    added sequence of gits

--- print.py
+++ print.py
@@ -3,3 +3,19 @@ print("hello world")
 print("test")
 
 print("this is test 1")
+
+
+print("init")
+print("set remote origin ")
+print("add ")
+print("commit")
+print("push ")
+
+
+
+print("clone")
+print("pull")
+print("add")
+print("commit")
+print("Push")
+
